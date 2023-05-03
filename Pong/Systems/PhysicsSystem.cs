e7c0848ec1a1a3807ab809369ed4c9789b5f218e
Jeff Anderson <foggeydoughtnut@gmail.com>
2023-05-02 20:56:12 -0600

    bounce is better, but not perfect

--- Pong/Systems/PhysicsSystem.cs
+++ Pong/Systems/PhysicsSystem.cs
@@ -70,13 +70,18 @@ namespace Systems
                         {
                             if (gameObject.Name.Contains("Player"))
                             {
-                                movableObject.GetComponent<Rigidbody>().Direction.X = -movableObject.GetComponent<Rigidbody>().Direction.X;
+                                // TODO check the direction vector of player and ball and calculate the projection vector based on those
+                                float yDirection = CalculateBallBounceYDirection(gameObject.GetComponent<Rigidbody>().Direction, movableObject.GetComponent<Rigidbody>().Direction, gameObject.GetComponent<Transform>().Position, movableObject.GetComponent<Transform>().Position, gameObject.GetComponent<BoxCollider>().Collider.Height/2);
+
+                                Vector2 direction = new(-movableObject.GetComponent<Rigidbody>().Direction.X, yDirection);
+                                direction.Normalize();
+
+                                movableObject.GetComponent<Rigidbody>().Direction = direction;
+
                                 if (gameObject.Name == "Player1") // Player 1 is on the left so move the ball to the right
                                     movableObject.GetComponent<Transform>().Position = new Vector2(gameObject.GetComponent<Transform>().Position.X + gameObject.GetComponent<BoxCollider>().Collider.Width, movableObject.GetComponent<Transform>().Position.Y);
-                                else
+                                else // Player 2 is on the right so move the ball to the left
                                     movableObject.GetComponent<Transform>().Position = new Vector2(gameObject.GetComponent<Transform>().Position.X - gameObject.GetComponent<BoxCollider>().Collider.Width, movableObject.GetComponent<Transform>().Position.Y);
-
-                                // TODO check the direction vector of player and ball and calculate the projection vector based on those
                             }
 
                             if (gameObject.Name == "border")
@@ -109,6 +114,37 @@ namespace Systems
                     }
                 }
             }
+        }
+
+        /// <summary>
+        /// Calculates the Y value of the bounce for the ball when it hits a player's paddle
+        /// </summary>
+        /// <param name="playerDirection"></param>
+        /// <param name="ballDirection"></param>
+        /// <returns>A y value between -1 and 1</returns>
+        private float CalculateBallBounceYDirection(Vector2 playerDirection, Vector2 ballDirection, Vector2 playerCenter, Vector2 hitPosition, float halfPaddleLength)
+        {
+            // Find distance away from center
+            float length = MathF.Sqrt(MathF.Pow(hitPosition.Y - playerCenter.Y, 2));
+            float percentageOfHalfPaddle = length / halfPaddleLength;
+            float yDirection = 1 - percentageOfHalfPaddle;
+
+            // If the ball is coming from the top then bounce back up if it hits above the half
+            if (ballDirection.Y > 0 && hitPosition.Y < playerCenter.Y)
+            {
+                yDirection *= -1;
+            }
+            // else bounce down
+
+            // If the ball is coming from the bottom then bounce back down if it hits below the half
+            else if (ballDirection.Y < 0 && hitPosition.Y > playerCenter.Y) 
+            {
+                yDirection *= -1;
+            }
+            // else bounce up
+
+
+            return yDirection;
         }
 
         /// <summary>
